f6df199e6622d9d0d11b97e236cb112a4b0fbedf
Iayoo <vilnius94@163.com>
2021-11-08 14:00:32 +0800

    新增日志ID

---
+++ src/monolog/mysql/Log.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Iayoo\monolog\mysql;
+
+use Exception;
+use Monolog\Logger;
+
+class Log
+{
+    /**
+     * Create a custom Monolog instance.
+     *
+     * @param  array $config
+     * @return Logger
+     * @throws Exception
+     */
+    public function __invoke(array $config)
+    {
+        $channel = $config['name'] ?? env('APP_ENV');
+        $monolog = new Logger($channel);
+        $monolog->pushHandler(new MysqlHandler());
+        return $monolog;
+    }
+}
